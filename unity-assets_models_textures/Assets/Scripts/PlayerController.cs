95a2c6a8ba616038536e2430b819253a23307c9f
bapt2 <113673417+bapt2@users.noreply.github.com>
2023-09-18 11:31:11 +0200

    adding PlayerMovement file and positionning the camera

---
+++ unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    public float jumpForce = 0;
+
+    public Vector3 boxSize;
+    public float maxDistance;
+    public LayerMask layerMask;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontale = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
+        rb.velocity = new Vector3(horizontale, 0, vertical) * speed * Time.fixedDeltaTime;
+        if (Input.GetKeyDown(KeyCode.Space) && GroundCheck())
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(transform.position - transform.up * maxDistance, boxSize);
+    }
+
+    bool GroundCheck()
+    {
+        if (Physics.BoxCast(transform.position, boxSize, -transform.up, transform.rotation, maxDistance, layerMask))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
